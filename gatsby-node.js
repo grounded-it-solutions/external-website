b99f4ff2138b476cb03d59d56c55eed627800927
trycrmr <trycrmr@gmail.com>
2019-10-22 09:55:11 -0400

    Refactors blog post page creation to properly resolve the promise

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,26 +16,32 @@ const converter = new markdownHtmlConverter.Converter()
 
 exports.createPages = ({ actions }) => {
   return new Promise((resolve, reject) => {
-    return fs.readdir(blogPostDirectories, (err, theBlogPostDirectories) => {
+    fs.readdir(blogPostDirectories, (err, theBlogPostDirectories) => {
       if(err) {
         reject(err)
       } else {
         try {
-          theBlogPostDirectories.forEach(aBlogPostDirectory => {
-            const blogPostMetadata = require(`${__dirname}/src/markdown/blogPosts/${aBlogPostDirectory}/meta.js`)
-            const blogPostContent = require(`${__dirname}/src/markdown/blogPosts/${aBlogPostDirectory}/content.js`)
-              const {createPage} = actions
-              createPage({
-                path: `${blogPostMetadata.title}`,
-                component: path.resolve('./src/templates/blogposts.js'),
-                // The context is passed as props to the component as well
-                // as into the component's GraphQL query.
-                context: {
-                  meta: blogPostMetadata,
-                  content: blogPostContent
-                },
-              })
-          });
+          let blogsParsed = 0;
+          while (theBlogPostDirectories.length > blogsParsed) {
+            console.log(`Parsing ${theBlogPostDirectories[blogsParsed]}`)
+            theBlogPostDirectories.forEach(aBlogPostDirectory => {
+              const blogPostMetadata = require(`${__dirname}/src/markdown/blogPosts/${aBlogPostDirectory}/meta.js`)
+              const blogPostContent = require(`${__dirname}/src/markdown/blogPosts/${aBlogPostDirectory}/content.js`)
+                const {createPage} = actions
+                createPage({
+                  path: `${blogPostMetadata.title}`,
+                  component: path.resolve('./src/templates/blogposts.js'),
+                  // The context is passed as props to the component as well
+                  // as into the component's GraphQL query.
+                  context: {
+                    meta: blogPostMetadata,
+                    content: blogPostContent
+                  },
+                })
+            });
+            blogsParsed++
+          }
+          console.log('All blogs parsed')
           resolve(true)
         } catch(err) {
           reject(err)
