702d6b60a7b39f19818647b7a2b4e4edf6ea4698
trycrmr <trycrmr@gmail.com>
2019-10-25 14:38:52 -0400

    Polishes the nav and adds a background image

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,21 +27,23 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <main>
-          {children}
-        </main>
-        <footer>
-          <SiteMap />
-        </footer>
+      <div className="gritLogoBackground">
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0
+          }}
+        >
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <main className="topNavBuffer">
+            {children}
+          </main>
+          <footer>
+            <SiteMap />
+          </footer>
+        </div>
       </div>
     </>
   )
