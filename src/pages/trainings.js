275c9e9c260a92300706bcda2441ce562d934179
trycrmr <trycrmr@gmail.com>
2019-10-25 17:55:21 -0400

    Adds training content with initial layout

--- src/pages/trainings.js
+++ src/pages/trainings.js
@@ -7,8 +7,20 @@ import SEO from "../components/seo"
 const TrainingsPage = () => (
   <Layout>
     <SEO title="Trainings" />
-    <h1>Hi from the trainings page</h1>
-    <Link to="/">Go back to the homepage</Link>
+    <section className="textCalloutSection">
+      <h2>Trainings</h2>
+      <h5>Providing comprehensive, pragmatic systems training to interested enthusiasts and IT professionals. The primary course content goal is accessible content for all skill levels that garners results in the workplace. Competencies include AWS offerings, DevOps tooling, and information technology best practices.</h5>
+    </section>
+    <section className="gridWrapper">
+      <div className="gridBox libraryBackground">
+        <h1>AWS DevOps for Front-end Developers!</h1>
+      </div>
+      <div className="gridBox coffeeBackground">
+        <h1>...and more coming soon!</h1>
+      </div>
+    </section>
+    <button>Contact Grounded IT Solutions about what trainings to produce next!</button>
+    <p>Trainings delivered through Udemy</p>
   </Layout>
 )
 
