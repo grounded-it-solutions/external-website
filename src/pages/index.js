6c40a58d62c3fd49e0555e7b081c465cfc3b6d56
trycrmr <trycrmr@gmail.com>
2019-10-25 10:44:56 -0400

    Initial cut of homepage responsive grid layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,21 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import ConnectedBlogRollup from "../components/blogrollup"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <section className="gridWrapper">
+      <div className="gridBox">
+        <Image src={require(`../images/logo.png`)}/>
+      </div>
+      <div className="gridBox">
+        <Image src={require(`../images/logo.png`)}/>
+      </div>
+    </section>
     <div>
-      <Image src={require(`../images/logo.png`)}/>
+      <ConnectedBlogRollup limit={5} />
     </div>
   </Layout>
 )
