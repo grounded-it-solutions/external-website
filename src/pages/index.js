c0a52c63bfe8f7463cb489ea33534f54fd839dcb
trycrmr <trycrmr@gmail.com>
2019-10-25 14:49:19 -0400

    Updates homepage limit to 3 from 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const IndexPage = () => (
       Maybe insert something here describing GrIT.
     </section>
     <div>
-      <ConnectedBlogRollup limit={5} />
+      <ConnectedBlogRollup limit={3} />
     </div>
   </Layout>
 )
