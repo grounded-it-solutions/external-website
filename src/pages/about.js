039255735972ef84175bea15962d8be55a6aeea3
trycrmr <trycrmr@gmail.com>
2019-10-29 17:59:27 -0400

    Initial cut of the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,14 @@ import SEO from "../components/seo"
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    Some sort of section thing. I'm thinking me on the right and content on the left. 
-    Founder - Grounded IT Solutions LLC was founded in 2019 by Terry Creamer, a passionate IT professional based in Washington D.C. .
+    <section className="infoSection center">
+      <h3>About Grounded IT Solutions</h3>
+      <p>After a decade of experience, Grounded IT Solutions LLC was founded in 2019 by <a href="https://terrycreamer.codes" target="_blank" alt="A link to Terry Creamer's website.">Terry Creamer</a>, a passionate IT professional based in Washington D.C. .  Before starting Grounded IT Solutions Terry was a Technical Lead for a contractor at the Department of State overseeing several practitioners and a portfolio of web applications in a web technologies  innovation office. Prior to that he cut his teeth in the professional world working with a large IT Contractor on projects across four Federal agencies in various roles for several years. Terry is an AWS Certified Solutions Architect Professional.</p>
+      <div className="center">
+        <button className="attnButton">Contact Grounded IT Solutions*</button>
+        <p className="footnote">*Will open a new page on a new site.</p>
+      </div>
+    </section>
   </Layout>
 )
 
