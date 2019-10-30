2b733c0ff60f4dfc3f939ee1afe6fc276a58fd5d
trycrmr <trycrmr@gmail.com>
2019-10-30 19:11:54 -0400

    Tweaks the headers

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,19 +11,19 @@ const IndexPage = () => (
     <section className="gridBanner deskBackground banner">
       <div className="bannerContent">
         <h1>Grounded IT</h1>
-        <h3>Pragmatic IT freelancing services and online trainings to help you get ahead</h3>
+        <h4>Pragmatic IT freelancing services and online trainings to help you get ahead</h4>
         <Link to="#offerings"><button>Offerings</button></Link>
       </div>
     </section>
     <section className="textCalloutSection sectionTitle">
       <h2>Who we are</h2><hr />
-      <h4><p>Grounded IT is an information technology consulting company looking to help IT professionals grow their skills and meet the goals of their IT portfolios. The rapid pace of IT innovation has organizations struggling to balance long-term investments with impact as soon as possible.</p></h4>
+      <h5><p>Grounded IT is an information technology consulting company looking to help IT professionals grow their skills and meet the goals of their IT portfolios. The rapid pace of IT innovation has organizations struggling to balance long-term investments with impact as soon as possible.</p></h5>
       <br />
       <h2>What we do</h2><hr />
-      <h4><p>Grounded IT's targeted, tailored, fixed-fee offerings are designed to provide IT organizations a boost towards achieving common IT initiatives in the industry. While teams are being assembled, Grounded IT's offerings can provide a foundation for educating and accelerating groups towards IT objectives without the costs and delays associated with hiring and onboarding. With Grounded IT's expertise in cloud services, the results can be quickly transferred to IT staff onsite to contiue the progress.</p></h4>
+      <h5><p>Grounded IT's targeted, tailored, fixed-fee offerings are designed to provide IT organizations a boost towards achieving common IT initiatives in the industry. While teams are being assembled, Grounded IT's offerings can provide a foundation for educating and accelerating groups towards IT objectives without the costs and delays associated with hiring and onboarding. With Grounded IT's expertise in cloud services, the results can be quickly transferred to IT staff onsite to contiue the progress.</p></h5>
       <br />
       <h2>Why we do it</h2><hr />
-      <h4><p>The diverse array of problems IT Managers face on a daily basis are both context-specific and consuming. Being buried in the day-to-day grind, it can be difficult to experiment with forward-thinking practices that could alleviate some of that pressure. Grounded IT recognizes that need and seeks to fill it, allowing IT organizations an avenue to level up widespread, progressive, IT practices. Hopefully, the options Grounded IT provides enables clients to outpace their competitors and deliver for their customers.</p></h4>
+      <h5><p>The diverse array of problems IT Managers face on a daily basis are both context-specific and consuming. Being buried in the day-to-day grind, it can be difficult to experiment with forward-thinking practices that could alleviate some of that pressure. Grounded IT recognizes that need and seeks to fill it, allowing IT organizations an avenue to level up widespread, progressive, IT practices. Hopefully, the options Grounded IT provides enables clients to outpace their competitors and deliver for their customers.</p></h5>
     </section>
     <section className="textCalloutSection sectionTitle">
       <h2>Offerings</h2>
