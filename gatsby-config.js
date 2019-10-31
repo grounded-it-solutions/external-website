e338fc2e5cf368ee24ecccc498a032b29b8c6607
trycrmr <trycrmr@gmail.com>
2019-10-30 20:03:50 -0400

    Hopefully resolves integration issue that passed the integration env but failed in prod. Putting default config back.

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: [],
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
