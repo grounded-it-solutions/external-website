378a76d35f7c18bf30e4d0d5dcccd25dfb4991e0
Terry Creamer <trycrmr@gmail.com>
2019-11-13 14:35:42 -0500

    Adds gatsby plugin to generate web manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,25 @@ module.exports = {
         }
       }
     },
-    'gatsby-plugin-robots-txt'
+    'gatsby-plugin-robots-txt',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Grounded IT Solutions",
+        short_name: "Grounded IT",
+        start_url: "/",
+        background_color: "#94A6E8",
+        theme_color: "#94A6E8",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
